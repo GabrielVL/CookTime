32133aee6abe733aa2d0ae4e65dee7ff1023f815
Yordan <yordanrd.u@gmail.com>
2020-07-15 02:40:19 -0600

    Habilitada opcion de loguearse
    Ahora para poder ingresar en la aplicacion hace falta un nombre de usuario que se encuentre dentro del Json

--- CookTime/Views/LogIn.xaml.cs
+++ CookTime/Views/LogIn.xaml.cs
@@ -3,23 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Json;
 
 namespace CookTime.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LogIn : ContentPage
     {
+
+        readonly List<User> users;
+        JsonArray pubCont = new JsonArray();
         public LogIn()
         {
             InitializeComponent();
         }
+        public async void Peticion()
+        {
+            String url = "http://192.168.100.2:8080/CookTime_Web_exploded/users";
 
+
+            WebClient nombre = new WebClient();
+            pubCont = (JsonArray)JsonArray.Parse(nombre.DownloadString(url));
+        }
+        public Boolean Comparar(String nombre)
+        {
+            Boolean answer = true;
+            for (int i = 0; i < pubCont.Count; i++)
+            {
+                answer = false;
+                try
+                {
+                    Result.Text = nombre;
+                    if (nombre.Equals(pubCont[i]["nombre"]))
+                    {
+                        answer = true;
+                        return answer;
+                    }
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("Error al iniciar sesión", "No se pudo completar la accion", "Reintentar");
+                }
+            }
+            return answer;
+        }
         async void MainPage(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new MainPageI()));
+            Peticion();
+            if (Comparar(Nombre.Text.ToString()))
+            {
+                await Navigation.PushModalAsync(new NavigationPage(new MainPageI()));
+            }
+            else
+            {
+                await DisplayAlert("Error al iniciar sesión", "El usuario no se encuentra en la base de datos", "Reintentar");
+            }
         }
         async void Registrar(object sender, EventArgs e)
         {
